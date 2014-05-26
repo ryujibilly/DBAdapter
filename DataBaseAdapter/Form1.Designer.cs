e81d830ed8640964206cdf490db295a201c2d691
Bowen <ryujibilly@qq.com>
2014-05-26 16:22:00 +0800

    Server�

--- DataBaseAdapter/Form1.Designer.cs
+++ DataBaseAdapter/Form1.Designer.cs
@@ -28,12 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.vListBox1 = new VIBlend.WinForms.Controls.vListBox();
+            this.vLabel1 = new VIBlend.WinForms.Controls.vLabel();
+            this.SuspendLayout();
+            // 
+            // vListBox1
+            // 
+            this.vListBox1.Location = new System.Drawing.Point(13, 102);
+            this.vListBox1.Name = "vListBox1";
+            this.vListBox1.RoundedCornersMaskListItem = ((byte)(15));
+            this.vListBox1.Size = new System.Drawing.Size(870, 266);
+            this.vListBox1.TabIndex = 0;
+            this.vListBox1.Text = "vListBox1";
+            this.vListBox1.VIBlendScrollBarsTheme = VIBlend.Utilities.VIBLEND_THEME.VISTABLUE;
+            this.vListBox1.VIBlendTheme = VIBlend.Utilities.VIBLEND_THEME.VISTABLUE;
+            // 
+            // vLabel1
+            // 
+            this.vLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.vLabel1.DisplayStyle = VIBlend.WinForms.Controls.LabelItemStyle.TextOnly;
+            this.vLabel1.Ellipsis = false;
+            this.vLabel1.ImageAlignment = System.Drawing.ContentAlignment.TopLeft;
+            this.vLabel1.Location = new System.Drawing.Point(12, 73);
+            this.vLabel1.Multiline = true;
+            this.vLabel1.Name = "vLabel1";
+            this.vLabel1.Size = new System.Drawing.Size(99, 23);
+            this.vLabel1.TabIndex = 1;
+            this.vLabel1.Text = " 运行信息";
+            this.vLabel1.TextAlignment = System.Drawing.ContentAlignment.TopLeft;
+            this.vLabel1.UseMnemonics = true;
+            this.vLabel1.VIBlendTheme = VIBlend.Utilities.VIBLEND_THEME.VISTABLUE;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(895, 503);
+            this.Controls.Add(this.vLabel1);
+            this.Controls.Add(this.vListBox1);
+            this.Font = new System.Drawing.Font("幼圆", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "DataBaseAdapter-CPL-LWD";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private VIBlend.WinForms.Controls.vListBox vListBox1;
+        private VIBlend.WinForms.Controls.vLabel vLabel1;
+
     }
 }
 
---
+++ DataBaseAdapter/Server/Client.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+namespace DataBaseAdapter.Server
+{
+    /// <summary>
+    /// 客户端会话信息类
+    /// </summary>
+    public class Client
+    {
+        Socket m_clientSocket;
+        public Client() { }
+        public Socket ClientSocket
+        {
+            get { return m_clientSocket; }
+            set { this.m_clientSocket = value; }
+        }
+    }
+}
