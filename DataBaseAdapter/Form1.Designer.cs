539563166c71e5d0c57d3d1a030cab1dee531a6c
Bowen <ryujibilly@qq.com>
2014-05-27 18:38:06 +0800

    527下班

--- DataBaseAdapter/Form1.Designer.cs
+++ DataBaseAdapter/Form1.Designer.cs
@@ -30,6 +30,16 @@
         {
             this.vListBox1 = new VIBlend.WinForms.Controls.vListBox();
             this.vLabel1 = new VIBlend.WinForms.Controls.vLabel();
+            this.vLabel2 = new VIBlend.WinForms.Controls.vLabel();
+            this.vComboBox1 = new VIBlend.WinForms.Controls.vComboBox();
+            this.vLabel3 = new VIBlend.WinForms.Controls.vLabel();
+            this.vTextBox1 = new VIBlend.WinForms.Controls.vTextBox();
+            this.vLabel4 = new VIBlend.WinForms.Controls.vLabel();
+            this.vTextBox2 = new VIBlend.WinForms.Controls.vTextBox();
+            this.vCheckBox1 = new VIBlend.WinForms.Controls.vCheckBox();
+            this.vButton1 = new VIBlend.WinForms.Controls.vButton();
+            this.vButton2 = new VIBlend.WinForms.Controls.vButton();
+            this.vGroupBox1 = new VIBlend.WinForms.Controls.vGroupBox();
             this.SuspendLayout();
             // 
             // vListBox1
@@ -37,7 +47,7 @@
             this.vListBox1.Location = new System.Drawing.Point(13, 102);
             this.vListBox1.Name = "vListBox1";
             this.vListBox1.RoundedCornersMaskListItem = ((byte)(15));
-            this.vListBox1.Size = new System.Drawing.Size(870, 266);
+            this.vListBox1.Size = new System.Drawing.Size(870, 236);
             this.vListBox1.TabIndex = 0;
             this.vListBox1.Text = "vListBox1";
             this.vListBox1.VIBlendScrollBarsTheme = VIBlend.Utilities.VIBLEND_THEME.VISTABLUE;
@@ -59,11 +69,176 @@
             this.vLabel1.UseMnemonics = true;
             this.vLabel1.VIBlendTheme = VIBlend.Utilities.VIBLEND_THEME.VISTABLUE;
             // 
+            // vLabel2
+            // 
+            this.vLabel2.BackColor = System.Drawing.Color.Transparent;
+            this.vLabel2.DisplayStyle = VIBlend.WinForms.Controls.LabelItemStyle.TextOnly;
+            this.vLabel2.Ellipsis = false;
+            this.vLabel2.ImageAlignment = System.Drawing.ContentAlignment.TopLeft;
+            this.vLabel2.Location = new System.Drawing.Point(13, 13);
+            this.vLabel2.Multiline = true;
+            this.vLabel2.Name = "vLabel2";
+            this.vLabel2.Size = new System.Drawing.Size(80, 25);
+            this.vLabel2.TabIndex = 2;
+            this.vLabel2.Text = "vLabel2";
+            this.vLabel2.TextAlignment = System.Drawing.ContentAlignment.TopLeft;
+            this.vLabel2.UseMnemonics = true;
+            this.vLabel2.VIBlendTheme = VIBlend.Utilities.VIBLEND_THEME.VISTABLUE;
+            // 
+            // vComboBox1
+            // 
+            this.vComboBox1.BackColor = System.Drawing.Color.White;
+            this.vComboBox1.DisplayMember = "";
+            this.vComboBox1.DropDownMaximumSize = new System.Drawing.Size(1000, 1000);
+            this.vComboBox1.DropDownMinimumSize = new System.Drawing.Size(10, 10);
+            this.vComboBox1.DropDownResizeDirection = VIBlend.WinForms.Controls.SizingDirection.Both;
+            this.vComboBox1.DropDownWidth = 116;
+            this.vComboBox1.Location = new System.Drawing.Point(99, 12);
+            this.vComboBox1.Name = "vComboBox1";
+            this.vComboBox1.RoundedCornersMaskListItem = ((byte)(15));
+            this.vComboBox1.Size = new System.Drawing.Size(116, 23);
+            this.vComboBox1.TabIndex = 3;
+            this.vComboBox1.Text = "vComboBox1";
+            this.vComboBox1.UseThemeBackColor = false;
+            this.vComboBox1.UseThemeDropDownArrowColor = true;
+            this.vComboBox1.ValueMember = "";
+            this.vComboBox1.VIBlendScrollBarsTheme = VIBlend.Utilities.VIBLEND_THEME.VISTABLUE;
+            this.vComboBox1.VIBlendTheme = VIBlend.Utilities.VIBLEND_THEME.VISTABLUE;
+            // 
+            // vLabel3
+            // 
+            this.vLabel3.BackColor = System.Drawing.Color.Transparent;
+            this.vLabel3.DisplayStyle = VIBlend.WinForms.Controls.LabelItemStyle.TextOnly;
+            this.vLabel3.Ellipsis = false;
+            this.vLabel3.ImageAlignment = System.Drawing.ContentAlignment.TopLeft;
+            this.vLabel3.Location = new System.Drawing.Point(222, 12);
+            this.vLabel3.Multiline = true;
+            this.vLabel3.Name = "vLabel3";
+            this.vLabel3.Size = new System.Drawing.Size(80, 25);
+            this.vLabel3.TabIndex = 4;
+            this.vLabel3.Text = "vLabel3";
+            this.vLabel3.TextAlignment = System.Drawing.ContentAlignment.TopLeft;
+            this.vLabel3.UseMnemonics = true;
+            this.vLabel3.VIBlendTheme = VIBlend.Utilities.VIBLEND_THEME.VISTABLUE;
+            // 
+            // vTextBox1
+            // 
+            this.vTextBox1.BackColor = System.Drawing.Color.White;
+            this.vTextBox1.BoundsOffset = new System.Drawing.Size(1, 1);
+            this.vTextBox1.ControlBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(39)))));
+            this.vTextBox1.DefaultText = "Empty...";
+            this.vTextBox1.Location = new System.Drawing.Point(309, 11);
+            this.vTextBox1.MaxLength = 32767;
+            this.vTextBox1.Name = "vTextBox1";
+            this.vTextBox1.PasswordChar = '\0';
+            this.vTextBox1.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.vTextBox1.SelectionLength = 0;
+            this.vTextBox1.SelectionStart = 0;
+            this.vTextBox1.Size = new System.Drawing.Size(112, 23);
+            this.vTextBox1.TabIndex = 5;
+            this.vTextBox1.Text = "vTextBox1";
+            this.vTextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.vTextBox1.VIBlendTheme = VIBlend.Utilities.VIBLEND_THEME.VISTABLUE;
+            // 
+            // vLabel4
+            // 
+            this.vLabel4.BackColor = System.Drawing.Color.Transparent;
+            this.vLabel4.DisplayStyle = VIBlend.WinForms.Controls.LabelItemStyle.TextOnly;
+            this.vLabel4.Ellipsis = false;
+            this.vLabel4.ImageAlignment = System.Drawing.ContentAlignment.TopLeft;
+            this.vLabel4.Location = new System.Drawing.Point(440, 13);
+            this.vLabel4.Multiline = true;
+            this.vLabel4.Name = "vLabel4";
+            this.vLabel4.Size = new System.Drawing.Size(80, 25);
+            this.vLabel4.TabIndex = 6;
+            this.vLabel4.Text = "vLabel4";
+            this.vLabel4.TextAlignment = System.Drawing.ContentAlignment.TopLeft;
+            this.vLabel4.UseMnemonics = true;
+            this.vLabel4.VIBlendTheme = VIBlend.Utilities.VIBLEND_THEME.VISTABLUE;
+            // 
+            // vTextBox2
+            // 
+            this.vTextBox2.BackColor = System.Drawing.Color.White;
+            this.vTextBox2.BoundsOffset = new System.Drawing.Size(1, 1);
+            this.vTextBox2.ControlBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(39)))));
+            this.vTextBox2.DefaultText = "Empty...";
+            this.vTextBox2.Location = new System.Drawing.Point(537, 11);
+            this.vTextBox2.MaxLength = 32767;
+            this.vTextBox2.Name = "vTextBox2";
+            this.vTextBox2.PasswordChar = '\0';
+            this.vTextBox2.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.vTextBox2.SelectionLength = 0;
+            this.vTextBox2.SelectionStart = 0;
+            this.vTextBox2.Size = new System.Drawing.Size(123, 23);
+            this.vTextBox2.TabIndex = 7;
+            this.vTextBox2.Text = "vTextBox2";
+            this.vTextBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.vTextBox2.VIBlendTheme = VIBlend.Utilities.VIBLEND_THEME.VISTABLUE;
+            // 
+            // vCheckBox1
+            // 
+            this.vCheckBox1.BackColor = System.Drawing.Color.Transparent;
+            this.vCheckBox1.Location = new System.Drawing.Point(686, 11);
+            this.vCheckBox1.Name = "vCheckBox1";
+            this.vCheckBox1.Size = new System.Drawing.Size(125, 24);
+            this.vCheckBox1.TabIndex = 8;
+            this.vCheckBox1.Text = "vCheckBox1";
+            this.vCheckBox1.UseVisualStyleBackColor = false;
+            this.vCheckBox1.VIBlendTheme = VIBlend.Utilities.VIBLEND_THEME.VISTABLUE;
+            // 
+            // vButton1
+            // 
+            this.vButton1.AllowAnimations = true;
+            this.vButton1.BackColor = System.Drawing.Color.Transparent;
+            this.vButton1.Location = new System.Drawing.Point(171, 53);
+            this.vButton1.Name = "vButton1";
+            this.vButton1.RoundedCornersMask = ((byte)(15));
+            this.vButton1.Size = new System.Drawing.Size(100, 30);
+            this.vButton1.TabIndex = 9;
+            this.vButton1.Text = "vButton1";
+            this.vButton1.UseVisualStyleBackColor = false;
+            this.vButton1.VIBlendTheme = VIBlend.Utilities.VIBLEND_THEME.VISTABLUE;
+            // 
+            // vButton2
+            // 
+            this.vButton2.AllowAnimations = true;
+            this.vButton2.BackColor = System.Drawing.Color.Transparent;
+            this.vButton2.Location = new System.Drawing.Point(481, 52);
+            this.vButton2.Name = "vButton2";
+            this.vButton2.RoundedCornersMask = ((byte)(15));
+            this.vButton2.Size = new System.Drawing.Size(100, 30);
+            this.vButton2.TabIndex = 10;
+            this.vButton2.Text = "vButton2";
+            this.vButton2.UseVisualStyleBackColor = false;
+            this.vButton2.VIBlendTheme = VIBlend.Utilities.VIBLEND_THEME.VISTABLUE;
+            // 
+            // vGroupBox1
+            // 
+            this.vGroupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.vGroupBox1.Location = new System.Drawing.Point(12, 344);
+            this.vGroupBox1.Name = "vGroupBox1";
+            this.vGroupBox1.Size = new System.Drawing.Size(871, 130);
+            this.vGroupBox1.TabIndex = 11;
+            this.vGroupBox1.TabStop = false;
+            this.vGroupBox1.Text = "vGroupBox1";
+            this.vGroupBox1.UseThemeBorderColor = true;
+            this.vGroupBox1.VIBlendTheme = VIBlend.Utilities.VIBLEND_THEME.VISTABLUE;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(895, 503);
+            this.Controls.Add(this.vGroupBox1);
+            this.Controls.Add(this.vButton2);
+            this.Controls.Add(this.vButton1);
+            this.Controls.Add(this.vCheckBox1);
+            this.Controls.Add(this.vTextBox2);
+            this.Controls.Add(this.vLabel4);
+            this.Controls.Add(this.vTextBox1);
+            this.Controls.Add(this.vLabel3);
+            this.Controls.Add(this.vComboBox1);
+            this.Controls.Add(this.vLabel2);
             this.Controls.Add(this.vLabel1);
             this.Controls.Add(this.vListBox1);
             this.Font = new System.Drawing.Font("幼圆", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
@@ -79,6 +254,16 @@
 
         private VIBlend.WinForms.Controls.vListBox vListBox1;
         private VIBlend.WinForms.Controls.vLabel vLabel1;
+        private VIBlend.WinForms.Controls.vLabel vLabel2;
+        private VIBlend.WinForms.Controls.vComboBox vComboBox1;
+        private VIBlend.WinForms.Controls.vLabel vLabel3;
+        private VIBlend.WinForms.Controls.vTextBox vTextBox1;
+        private VIBlend.WinForms.Controls.vLabel vLabel4;
+        private VIBlend.WinForms.Controls.vTextBox vTextBox2;
+        private VIBlend.WinForms.Controls.vCheckBox vCheckBox1;
+        private VIBlend.WinForms.Controls.vButton vButton1;
+        private VIBlend.WinForms.Controls.vButton vButton2;
+        private VIBlend.WinForms.Controls.vGroupBox vGroupBox1;
 
     }
 }
